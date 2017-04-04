51db357778a367e68a0d885c9c46585c300a8ecb
F541U <F541U@DESKTOP-27T6CPM>
2017-04-04 12:27:47 +0100

    Criação do branch modelo

---
+++ Vets/Vets/Models/Donos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vets.Models
+{
+    public class Donos
+    {
+        public int DonosID{ get; set;}
+
+        public int Nome { get; set;}
+
+        public string NIF { get; set;}
+
+        public string codPost { get; set;}
+
+        public string Morada { get; set;}
+
+        public double Altua { get; set;}
+
+        public int Idade { get; set;}
+
+
+
+    }
+}
---
+++ Vets/Vets/Models/VetsDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vets.Models
+{
+    public class VetsDB : DbContext {
+      //representar a Base de Dados
+      //descrever as tabelas que lá estão contidas
+      
+      //representar o 'construtor' desta classe
+      //indetifica onde se encontra a Base de Dados
+      public VetsDB() : base("VetsDBConnection"){ }
+    
+      //descrever as tabelas que estão na base de dados
+      public virtual DbSet<Donos> Donos { get; set; }
+    }
+}
